f1415fc634c2681a9b4b9062ae0ba7b4067a7d6b
EvilPerfectionist <EvilPerfectionist@users.noreply.github.com>
2017-12-11 14:25:35 +0800

    Syntax if not

--- OpenCVPython/OpenCVTest.py
+++ OpenCVPython/OpenCVTest.py
@@ -3,11 +3,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # normal distribution center at x=0 and y=5
-#x = np.random.randn(100000)
-#y = np.random.randn(100000) + 10
-x = [1, 2, 3, 2]
-y = [1, 2, 3, 2]
-
-plt.hist2d(x, y, bins=40, norm=LogNorm())
-plt.colorbar()
-plt.show()
+a = 0
+x = [4, 5, 6]
+if not a:
+    x = [1, 2, 3]
+print(x)
