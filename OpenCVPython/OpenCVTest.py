4dac95453020d5792336c37f83062c0f824cbbba
EvilPerfectionist <EvilPerfectionist@users.noreply.github.com>
2017-12-12 14:44:34 +0800

    fig.canvas.tostring_rgb

--- OpenCVPython/OpenCVTest.py
+++ OpenCVPython/OpenCVTest.py
@@ -1,10 +1,39 @@
-from matplotlib.colors import LogNorm
-import matplotlib.pyplot as plt
+"""
+Use backend agg to access the figure canvas as an RGB string and then
+convert it to an array and pass it to Pillow for rendering.
+"""
+
 import numpy as np
 
-# normal distribution center at x=0 and y=5
-a = 0
-x = [4, 5, 6]
-if not a:
-    x = [1, 2, 3]
-print(x)
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_agg import FigureCanvasAgg
+
+
+try:
+    from PIL import Image
+except ImportError:
+    raise SystemExit("Pillow must be installed to run this example")
+
+plt.plot([1, 2, 3])
+
+canvas = plt.get_current_fig_manager().canvas
+
+agg = canvas.switch_backends(FigureCanvasAgg)
+agg.draw()
+s = agg.tostring_rgb()
+
+# get the width and the height to resize the matrix
+l, b, w, h = agg.figure.bbox.bounds
+w, h = int(w), int(h)
+
+X = np.fromstring(s, np.uint8)
+X.shape = h, w, 3
+
+try:
+    im = Image.fromstring("RGB", (w, h), s)
+except Exception:
+    im = Image.frombytes("RGB", (w, h), s)
+
+# Uncomment this line to display the image using ImageMagick's
+# `display` tool.
+im.show()
