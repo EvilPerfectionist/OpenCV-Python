ddf7b3c0e237e0da382594219b9700600bafff38
EvilPerfectionist <EvilPerfectionist@users.noreply.github.com>
2017-12-16 21:04:41 +0800

    2017.12.16.21.04

--- OpenCVPython/Test.py
+++ OpenCVPython/Test.py
@@ -9,7 +9,7 @@ Z = np.float32(Z)
 
 # define criteria, number of clusters(K) and apply kmeans()
 criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
-K = 8
+K = 2
 ret,label,center=cv2.kmeans(Z,K,None,criteria,10,cv2.KMEANS_RANDOM_CENTERS)
 print(center)
 print(label)
@@ -19,6 +19,6 @@ res = center[label.flatten()]
 print(res)
 res2 = res.reshape((img.shape))
 
-cv2.imshow('origin',img)
+cv2.imshow('res2',res2)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
